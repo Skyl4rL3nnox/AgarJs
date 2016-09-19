00deb1597b64e643c1a31267b2d076a159f01549
Skyl4rL3nnox <skylarlennox1@outlook.com>
2016-09-20 01:17:33 +0200

    Update agar.js

--- bin/agar.js
+++ bin/agar.js
@@ -5,4 +5,4 @@
   @date: 20-09-2016
 */
 
-if('undefined'==typeof(agarjs)){window.agarjs=initializeAgarjs();}function initializeAgarjs(){var agarjs={};agarjs.start=function(){var a=1; var b=1+1; var c=a*b+1;startAgarjs(a,b,c);}return agarjs;}function startAgarjs(A_a,B_b,C_c){console.warn('Agarjs Started');}
+if('undefined'==typeof(agarjs)){window.agarjs=initializeAgarjs();}function initializeAgarjs(window){var agarjs={};agarjs.start=function(){var a=1; var b=1+1; var c=a*b+1;startAgarjs(a,b,c);}return agarjs;}(window);function startAgarjs(A_a,B_b,C_c){console.warn('Agarjs Started');}
