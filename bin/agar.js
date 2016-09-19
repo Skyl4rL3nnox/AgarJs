fdd833b58e9078aedceb48d63b967665689217ad
Skyl4rL3nnox <skylarlennox1@outlook.com>
2016-09-20 01:14:17 +0200

    Update agar.js

--- bin/agar.js
+++ bin/agar.js
@@ -1,1 +1,8 @@
+/*
+  (c) Copyright AgarJs
+  
+  @author: Skylar Lennox
+  @date: 20-09-2016
+*/
 
+if('undefined'==typeof(agarjs)){window.agarjs=initializeAgarjs();}function initializeAgarjs(){var agarjs={};agarjs.start=function(){var a=1; var b=1+1; var c=a*b+1;startAgarjs(a,b,c);}return agarjs;}function startAgarjs(A_a,B_b,C_c){console.warn('Agarjs Started');}
